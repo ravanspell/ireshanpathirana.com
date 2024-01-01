dcd2515de27a4f140055cf19a9277b50ce77ccc8
“ireshan” <ireshandj2@gmail.com>
2024-01-01 12:35:49 +0800

    about me section component initial draft

---
+++ src/app/organisms/AboutMeSection/AboutMeSection.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/your-renderer';
+
+import AboutMeSection from './AboutMeSection';
+
+const meta: Meta<typeof AboutMeSection> = {
+  component: AboutMeSection,
+};
+export default meta;
+
+type Story = StoryObj<typeof AboutMeSection>;
+
+
+export const Primary: Story = {};
