a572af29a355ee5183bd316eb5b791f2de47062a
“ireshan” <ireshandj2@gmail.com>
2024-01-02 01:51:31 +0800

    fix style issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './layout.scss'
+import '../styles/style.scss';
 
 const inter = Inter({ subsets: ['latin'] })
 
