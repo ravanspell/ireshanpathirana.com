0b53da0d7491bf3717e411d49b978061ce6c6e62
“ireshan” <ireshandj2@gmail.com>
2024-01-03 12:05:25 +0800

    main content basic template

---
+++ src/app/templates/MainContent/MainContent.tsx
@@ -0,0 +1,16 @@
+/**
+ * About me section
+ */
+import AboutMeSection from '@/app/organisms/AboutMeSection/AboutMeSection';
+import './MainContent.scss';
+
+
+const MainContent = (): JSX.Element => {
+    return (
+        <main>
+            <AboutMeSection />
+        </main>
+    );
+}
+
+export default MainContent;
