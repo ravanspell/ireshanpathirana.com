936d42344c91d74787075c8af8c1f95a6a46ba9e
“ireshan” <ireshandj2@gmail.com>
2024-06-24 23:30:57 +0800

    container layout settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className='container'>
+        <div className='container max-w-screen-xl'>
           {children}
         </div>
       </body>
