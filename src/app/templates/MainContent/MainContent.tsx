6e7d575fc1d6356578806a3bcd22206b4f265d7c
“ireshan” <ireshandj2@gmail.com>
2024-06-27 22:45:04 +0800

    main content background

--- src/app/templates/MainContent/MainContent.tsx
+++ src/app/templates/MainContent/MainContent.tsx
@@ -7,7 +7,7 @@ import ProjectsSection from '@/app/organisms/ProjectsSection/ProjectsSection';
 
 const MainContent = ({mainContentRef}:{mainContentRef: any}): JSX.Element => {
     return (
-        <main>
+        <main className='bg-custom-eerie-black-2 border border-custom-jet rounded-lg p-4 shadow-custom-shadow-1 z-10'>
             <AboutMeSection mainContentRef={mainContentRef}/>
             <ProjectsSection mainContentRef={mainContentRef}/>
         </main>
