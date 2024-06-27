a5e78b44e24ee585f29b05006f6adad092f77f37
“ireshan” <ireshandj2@gmail.com>
2024-06-27 22:46:18 +0800

    setup tailwind custom css properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,12 @@ module.exports = {
       colors: {
         'gradient-onyx': 'var(--bg-gradient-onyx)',
         'orange-yellow-crayola': 'var(--orange-yellow-crayola)',
-      }
+        'custom-eerie-black-2': 'var(--eerie-black-2)',
+        'custom-jet': 'var(--jet)',
+      },
+      boxShadow: {
+        'custom-shadow-1': 'var(--shadow-1)',
+      },
     },
   },
   plugins: [],
