7fe755e9d7f09831760191f97ff66e53f8766865
“ireshan” <ireshandj2@gmail.com>
2024-07-02 22:16:37 +0800

    layour re arrange

--- src/app/templates/MainContent/MainContent.tsx
+++ src/app/templates/MainContent/MainContent.tsx
@@ -8,7 +8,7 @@ import ProjectsSection from '@/app/organisms/ProjectsSection/ProjectsSection';
 
 const MainContent = ({mainContentRef}:{mainContentRef: any}): JSX.Element => {
     return (
-        <main className='bg-custom-eerie-black-2 border border-custom-jet rounded-lg p-4 shadow-custom-shadow-1 z-10'>
+        <main className='bg-custom-eerie-black-2 border border-custom-jet rounded-lg p-8 shadow-custom-shadow-1 z-10'>
             <AboutMeSection mainContentRef={mainContentRef}/>
             <ProjectsSection mainContentRef={mainContentRef}/>
             <ExperinceSection mainContentRef={mainContentRef}/>
