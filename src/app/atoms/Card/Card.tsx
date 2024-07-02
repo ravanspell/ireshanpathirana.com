32696ac6ef0c5d206868c9b3d81db650e9c6bb5c
“ireshan” <ireshandj2@gmail.com>
2024-07-03 00:09:22 +0800

    re style and refactor card component

--- src/app/atoms/Card/Card.tsx
+++ src/app/atoms/Card/Card.tsx
@@ -12,12 +12,32 @@ export interface CardProps {
      * for unit tests and custom styles
      */
     id: string
+    /**
+     * pass the custom styles
+     */
+    className?: string
 }
 
 const Card = (props: CardProps): JSX.Element => {
-    const { children, id} = props;
+    const { children, id, className = '' } = props;
     return (
-        <div id={id} className="card">
+        <div
+            id={id}
+            className={`${className}
+                relative
+                bg-border-gradient-onyx
+                p-4 pt-[45px]
+                rounded-[14px]
+                shadow-shadow-2
+                cursor-pointer
+                z-10
+                before:content-['']
+                before:absolute
+                before:inset-[1px]
+                before:bg-gradient-jet
+                before:rounded-[14px]
+                before:z-[-1]`
+            }>
             {children}
         </div>
     );
