d85bf0c297c43816f7123c77687d068d7908ba49
“ireshan” <ireshandj2@gmail.com>
2024-07-21 23:28:40 +0800

    Nav bar modifications for medium and large screen

--- src/app/organisms/NavBar/NavBar.tsx
+++ src/app/organisms/NavBar/NavBar.tsx
@@ -1,58 +1,48 @@
-"use client"
-/**
- * Main nav Bar
- */
-import { useEffect, useRef, useState } from 'react';
-import MainMenuItems from '@/app/molecules/MainMenuItems/MainMenuItems';
+import MainMenuItems from "@/app/molecules/MainMenuItems/MainMenuItems";
 
-const NavBar = ({ mainContentRef }: { mainContentRef: any }): JSX.Element => {
-
-    const observers = useRef<Record<string, IntersectionObserver>>({});
-    const [visibleKey, setVisibleKey] = useState<string>('')
-
-    const observerCallback = async (e: any, key: string) => {
-        if (e.length && e[0].isIntersecting) {
-            setVisibleKey(key);
-        }
-    };
-
-    useEffect(() => {
-        console.log("mainContentRef.current", mainContentRef.current);
-
-
-        if (Object.keys(mainContentRef.current).length > 0 && observers.current) {
-            console.log("mainContentRef.current", mainContentRef.current);
-            let options = {  
-                // fire intersect observer when 100% of element is inside of the view.
-                threshold: 1.0,
-            }
-            const currentObservers = observers.current;
-            Object.keys(mainContentRef.current).forEach((sectionKey: string) => {
-                currentObservers[sectionKey] = new IntersectionObserver((e) =>
-                    observerCallback(e, sectionKey),
-                    options
-                );
-                if (mainContentRef.current[sectionKey]) {
-                    currentObservers[sectionKey].observe(mainContentRef.current[sectionKey]);
-                }
-            })
-        }
-
-        return () => {
-            if (observers.current) {
-                const currentObservers = observers.current;
-                Object.keys(observers.current).forEach((ObserverKey: string) => currentObservers[ObserverKey].disconnect());
-            }
-        }
-
-    }, [mainContentRef, observers])
+const NavBar = (): JSX.Element => {
 
     return (
-        <header>
-            <nav className='nav-bar'>
-               <MainMenuItems visibleKey={visibleKey} />
-            </nav>
-        </header>
+        <nav className="
+        fixed
+        md:relative
+        p-5
+        md:p-0
+        bottom-0
+        left-0 
+        backdrop-blur
+        md:backdrop-blur-none
+        bg-nav-bar-background
+        md:bg-transparent
+        rounded-t-[12px]
+        md:rounded-none
+        border
+        md:border-none
+        border-custom-jet 
+        w-full 
+        flex 
+        flex-wrap 
+        justify-center 
+        align-middle 
+        z-50">
+            <ul className="flex md:flex-col items-center gap-8 md:gap-3">
+                <MainMenuItems menuItems={[
+                    {
+                        href: '#about',
+                        label: 'About'
+                    },
+                    {
+                        href: '#experince',
+                        label: 'Experince'
+                    },
+                    {
+                        href: '#services',
+                        label: 'Services'
+                    }
+                ]}
+                />
+            </ul>
+        </nav>
     );
 }
 
